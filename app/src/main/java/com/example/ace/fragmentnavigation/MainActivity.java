76ed7f21b139baf55c36ad2c232205d1d463272b
piali saha <pialisaha@gmail.com>
2017-01-17 17:21:24 +0000

    adding one more comment

--- app/src/main/java/com/example/ace/fragmentnavigation/MainActivity.java
+++ app/src/main/java/com/example/ace/fragmentnavigation/MainActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
+//Piali
 public class MainActivity extends AppCompatActivity {
     String fragmentNameTag;
     FragmentOne mfragmentOne;
